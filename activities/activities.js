27e9b0c08ce24f6bfafba19c88e135dd35d35e59
AlejaWC <ing.biologica.wongland@gmail.com>
2025-07-07 22:53:58 -0500

    Activities views

---
+++ activities/activities.js
@@ -0,0 +1,9 @@
+const openModal = () => {
+  const modal = document.getElementById("modal");
+  modal.style.display = "flex";
+};
+
+const closeModal = () => {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+};
